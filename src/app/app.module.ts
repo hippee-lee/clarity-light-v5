e836a8a49daa7dfb122ed78a93ee04bad82eb93a
Matt Hippely <mhippely@vmware.com>
2021-02-17 13:23:50 -0800

    Add and config core for simple Angualr app
    Signed-off-by: Matt Hippely <mhippely@vmware.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import '@cds/core/alert/register.js';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
